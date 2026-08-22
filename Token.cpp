f070c8bd81cb27c09ef614d2525f0e9c700e27f3
FardeenNahian <nahianfardeen@gmail.com>
2026-08-23 02:27:18 +0600

    Token Type Names

--- Token.cpp
+++ Token.cpp
@@ -1,41 +1,42 @@
-#ifndef TOKEN_HPP
-#define TOKEN_HPP
+#include "Token.hpp"
 
-enum class TokenType {
-    Number,
-    String,
-    Identifier,
+const char* tokenTypeName(TokenType type) {
+    switch (type) {
+        case TokenType::Number: return "NUMBER";
+        case TokenType::String: return "STRING";
+        case TokenType::Identifier: return "IDENTIFIER";
 
-    KwInt,
-    KwString,
-    KwPrint,
-    KwIf,
-    KwElse,
-    KwWhile,
+        case TokenType::KwInt: return "KW_INT";
+        case TokenType::KwString: return "KW_STRING";
+        case TokenType::KwPrint: return "KW_PRINT";
+        case TokenType::KwIf: return "KW_IF";
+        case TokenType::KwElse: return "KW_ELSE";
+        case TokenType::KwWhile: return "KW_WHILE";
 
-    Plus,
-    Minus,
-    Star,
-    Slash,
+        case TokenType::Plus: return "PLUS";
+        case TokenType::Minus: return "MINUS";
+        case TokenType::Star: return "STAR";
+        case TokenType::Slash: return "SLASH";
 
-    Equal,
-    EqualEqual,
-    BangEqual,
+        case TokenType::Equal: return "EQUAL";
+        case TokenType::EqualEqual: return "EQUAL_EQUAL";
+        case TokenType::BangEqual: return "BANG_EQUAL";
 
-    Less,
-    LessEqual,
-    Greater,
-    GreaterEqual,
+        case TokenType::Less: return "LESS";
+        case TokenType::LessEqual: return "LESS_EQUAL";
+        case TokenType::Greater: return "GREATER";
+        case TokenType::GreaterEqual: return "GREATER_EQUAL";
 
-    LParen,
-    RParen,
-    LBrace,
-    RBrace,
+        case TokenType::LParen: return "LPAREN";
+        case TokenType::RParen: return "RPAREN";
+        case TokenType::LBrace: return "LBRACE";
+        case TokenType::RBrace: return "RBRACE";
 
-    Semicolon,
-    Newline,
-    EndOfFile,
-    Unknown
-};
+        case TokenType::Semicolon: return "SEMICOLON";
+        case TokenType::Newline: return "NEWLINE";
+        case TokenType::EndOfFile: return "EOF";
+        case TokenType::Unknown: return "UNKNOWN";
+    }
 
-#endif
+    return "UNKNOWN";
+}
