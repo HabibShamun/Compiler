72c8f98baeecb63be82f9d83730f024b6dc77a3c
Habib Shamun <habibsalehinshamun@gmail.com>
2026-08-23 14:01:02 +0600

    parser: handling  declarations and simple statements hpp

--- parser.hpp
+++ parser.hpp
@@ -3,6 +3,7 @@
 #include "AST.hpp"
 #include "Token.hpp"
 
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -14,12 +15,37 @@ public:
     const std::vector<std::string>& errors() const;
 
 private:
+    struct ParseError : std::runtime_error {
+        using std::runtime_error::runtime_error;
+    };
+
     std::vector<Token> tokens_;
     std::size_t pos_ = 0;
     std::vector<std::string> errors_;
 
     const Token& current() const;
+    const Token& previous() const;
     bool isAtEnd() const;
+    bool check(TokenType type) const;
+    bool match(TokenType type);
     const Token& advance();
+    const Token& expect(TokenType type, const std::string& message);
+
+    void errorAt(const Token& token, const std::string& message);
+    void synchronize();
+    void consumeTerminators();
+    void consumeStatementEnd(const std::string& context);
+
+    StmtPtr parseDeclarationOrStatement();
+    StmtPtr parseVarDeclaration();
+    StmtPtr parseStatement();
+    StmtPtr parseAssignment();
+    StmtPtr parsePrint();
+
+    ExprPtr parseExpression();
+    ExprPtr parsePrimary();
+
+    static bool isTypeToken(TokenType type);
+    static ValueType typeFromToken(TokenType type);
 };
 
